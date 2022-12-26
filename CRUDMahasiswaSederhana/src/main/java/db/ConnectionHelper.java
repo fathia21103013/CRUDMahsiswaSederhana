0259bdfdf59fd032b5cf52949fad2942185862a6
fathia21103013 <114973187+fathia21103013@users.noreply.github.com>
2022-12-26 20:15:01 +0700

    Add files via upload

---
+++ CRUDMahasiswaSederhana/src/main/java/db/ConnectionHelper.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ConnectionHelper {
+    public static Connection connect;
+    public static Statement st;
+    public static ResultSet rs;
+    
+    public static Connection getConnection() throws SQLException{
+        String db = "sisteminformasi";
+        String url = "jbdc:mysql://localhost:3306/"+db;
+        String username = "root";
+        String pass = "";
+        if(connect==null){
+            try{
+                System.out.println("Membuat koneksi...");
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connect=DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/sisteminformasi",
+                        "root",
+                        "");
+                st = connect.createStatement();
+                JOptionPane.showMessageDialog(null,"Terkoneksi");
+                System.out.println("Koneksi berhasil.");
+            }catch(ClassNotFoundException | SQLException e){
+                JOptionPane.showMessageDialog(null,"Tidak Terkoneksi");
+                System.out.println("Koneksi berhasil.");
+            }
+        }
+        return connect;
+    }
+
+    public static void main(String[] args) throws SQLException {
+        ConnectionHelper.getConnection();
+    }
+}
